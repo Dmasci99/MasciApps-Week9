d783485e1902736e6af5801e590ea27056bcdde1
Dan Masci <minimasci@yahoo.ca>
2016-07-21 09:47:35 -0400

    Initial Commit -New DB Created -Controllers/Views/Models copied from MasciApps-Store

---
+++ MasciApps-Week9/Controllers/StoreController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MasciApps_Week9.Models;
+
+namespace MasciApps_Week9.Controllers
+{
+    public class StoreController : Controller
+    {
+        //
+        // GET: /Store/
+        public ActionResult Index()
+        {
+            List<Genre> genres = new List<Genre>
+            {
+                new Genre("Disco"),
+                new Genre("Jazz"),
+                new Genre("Rock")
+            };
+            return View(genres);
+        }
+        //
+        // GET: /Store/Browse?genre=jazz
+        public ActionResult Browse(string genre)
+        {
+            Genre genreModel = new Genre(genre);
+            return View(genreModel);
+        }
+        //
+        // GET: /Store/Details
+        public ActionResult Details(int id)
+        {
+            Album album = new Album {
+                Title = "Album " + id
+            };
+            return View(album);
+        }
+    }
+}
---
+++ MasciApps-Week9/Models/Genre.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MasciApps_Week9.Models
+{
+    public class Genre
+    {
+        private string v;
+
+        public Genre(string Name)
+        {
+            this.Name = Name;
+        }
+
+        public string Name { get; set; }
+    }
+}
